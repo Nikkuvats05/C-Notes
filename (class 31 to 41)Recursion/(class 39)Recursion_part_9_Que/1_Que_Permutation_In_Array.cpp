f84653ae69d3e88d0714507c875c5f569343f5ee
Nikkuvats05 <nikhilvats208@gmail.com>
2026-08-06 23:55:48 +0530

    permutation in array by recursion

---
+++ (class 31 to 41)Recursion/(class 39)Recursion_part_9_Que/1_Que_Permutation_In_Array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void solve(vector<int> nums, vector<vector<int>>&ans, int index){
+    if(index>=nums.size()){
+        ans.push_back(nums);
+        return;
+    }
+
+    for(int j= index; j<nums.size(); j++){
+        swap(nums[index], nums[j]);
+        solve(nums, ans, index+1);
+        //BACKTRACK
+         swap(nums[index], nums[j]);
+    }
+}
+int main(){
+    vector<int> nums ={1,2,3};
+    vector<vector<int>>ans;
+    int index=0;
+    solve(nums, ans, index);
+    for(int i=0; i<ans.size(); i++){
+        for(int j=0; j<ans[0].size(); j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
