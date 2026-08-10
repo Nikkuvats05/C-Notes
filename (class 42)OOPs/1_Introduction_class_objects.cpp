aa93e0c2545def2d52e9082c1c9052d87372cf9f
Nikkuvats05 <nikhilvats208@gmail.com>
2026-08-10 23:58:14 +0530

    intro to OOPs

---
+++ (class 42)OOPs/1_Introduction_class_objects.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class hero{
+   
+    //Properties
+    int health;
+    char level;
+
+};
+int main(){
+    hero h1;
+    cout<<"size of hero hi is = "<<sizeof(h1)<<endl;
+
+}
